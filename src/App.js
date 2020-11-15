34dd24910b0641d17da67c99019f18a614a11c1c
tasnim-nova <tasnimtareq.cu@gmail.com>
2020-11-15 20:01:15 +0600

    dynamic data passing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks =['Ananta', 'Jashim', 'Kanchon']
+  const hobbys =['Traveling', 'teaching', 'social work']
   return (
     <div className="App">
-            <Nayok></Nayok>
-            <Nayok></Nayok>
-            <Nayok></Nayok>
-            <Nayok></Nayok>
-            <Nayok></Nayok>
-            <Nayok></Nayok>
+            <Nayok name='shakib Khan' hobby='dancing'></Nayok>
+            <Nayok name='srk' hobby='boxing'></Nayok>
+            <Nayok name='dipjol' hobby='fighting'></Nayok>
+            <Nayok name={nayoks[0]} hobby={hobbys[0]}></Nayok>
+            <Nayok name={nayoks[1]} hobby={hobbys[1]}></Nayok>
+            <Nayok name={nayoks[2]} hobby={hobbys[2]}></Nayok>
             <Nayok></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,14 +21,16 @@ function App() {
   );
 }
 
-function Nayok(prop) {
+function Nayok(props) {
   const nayokStyle = {
     border: '2px solid purple',
     margin: '20px',
   }
-  return <div style={nayokStyle}>
-     <h1>I'm Hero</h1>
-     <p>My Hobby is Singing</p>
+  return( 
+    <div style={nayokStyle}>
+     <h1>I'm {props.name}</h1>
+     <p>My Hobby is { props.hobby}</p>
      </div>
+     )
 }
 export default App;
