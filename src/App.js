a6011cd5f61a2dc6df9c2f2f41dc0944fc3107da
tasnim-nova <tasnimtareq.cu@gmail.com>
2020-11-15 19:34:38 +0600

    basics of passing data using react and some demo practice

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+            <Nayok></Nayok>
+            <Nayok></Nayok>
+            <Nayok></Nayok>
+            <Nayok></Nayok>
+            <Nayok></Nayok>
+            <Nayok></Nayok>
+            <Nayok></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
 }
 
+function Nayok(prop) {
+  const nayokStyle = {
+    border: '2px solid purple',
+    margin: '20px',
+  }
+  return <div style={nayokStyle}>
+     <h1>I'm Hero</h1>
+     <p>My Hobby is Singing</p>
+     </div>
+}
 export default App;
